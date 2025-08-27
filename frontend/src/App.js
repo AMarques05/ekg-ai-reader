bfc4a49dd90e7d626a9e5aeff82b53ed882bc3d7
AMarques05 <05marquesanthony@gmail.com>
2025-08-27 12:22:24 -0400

    began styling and initial react components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
+import AppContainer from './components/AppContainer.jsx';
 
 function App() {
   return (
-    <div className="App min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-md">
-        <h1 className="text-3xl font-bold text-blue-600 mb-4">EKG AI Reader</h1>
-        <p className="text-gray-600">Your EKG analysis application</p>
-      </div>
-    </div>
+    <AppContainer />
   );
 }
 
---
+++ frontend/src/components/AppContainer.jsx
@@ -0,0 +1,12 @@
+
+function AppContainer(){
+    return (
+    <div className="App min-h-screen flex items-center justify-center bg-gray-900">
+      <div className="bg-gray-700 p-8 rounded-lg shadow-lg shadow-cyan-500/50">
+        <h1 className="text-2x1 font-bold text-white">EKG-AI-Reader</h1>
+      </div>
+    </div>
+    )
+}
+
+export default AppContainer;
