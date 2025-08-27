d55586bfa4c1d151d16fd9076c25ebd59d04fe3d
AMarques05 <05marquesanthony@gmail.com>
2025-08-27 16:57:52 -0400

    additional styling

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,6 +1,10 @@
 
 function Input() {
-
+    return (
+        <button className = "h-1/5 w-full border-2 rounded border-blue-600 bg-blue-300 text-blue-600">
+            Proccess
+        </button>
+    )
 }
 
 export default Input;
