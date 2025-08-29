9a852d5abe70401f2242c124fce1e34355dca363
AMarques05 <05marquesanthony@gmail.com>
2025-08-28 21:01:34 -0400

    additional styling added

--- frontend/src/components/GraphContainer.jsx
+++ frontend/src/components/GraphContainer.jsx
@@ -1,7 +1,7 @@
 
 function GraphContainer(){
     return (
-        <div className = "flex flex-col justify-center items-center col-span-2 row-span-2 bg-gray-400">
+        <div className = "flex flex-col justify-center items-center col-span-2 row-span-2 border-2 border-dashed border-gray-400 rounded-lg">
 
         </div>
     )
