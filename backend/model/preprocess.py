6102071f0753db922af1c352df0f15741f30c241
AMarques05 <05marquesanthony@gmail.com>
2025-09-05 10:31:44 -0400

    more comments!!

--- backend/model/preprocess.py
+++ backend/model/preprocess.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.signal import butter, filtfilt, iirnotch, resample
 
+#Ensures to keep only the frequencies between 0.5 to 40hz to prevent confustion from R-Peaks to RR-Interval
 def bandpass(signal, fs, lowcut=0.5, highcut=40.0, order=4):
     nyq = 0.5 * fs
     low = lowcut / nyq
@@ -8,23 +9,27 @@ def bandpass(signal, fs, lowcut=0.5, highcut=40.0, order=4):
     b, a = butter(order, [low, high], btype='band')
     return filtfilt(b, a, signal)
 
+#Filter process to cut our powerline interface
 def notch_filter(signal, fs, freq=60.0, Q=30.0):
     nyq = 0.5 * fs
     w0 = freq / nyq
     b, a = iirnotch(w0, Q)
     return filtfilt(b, a, signal)
 
+#Standardizs our signal between 0 and 1 no matter the raw amplitude
 def standardize(signal):
     mean = np.mean(signal)
     std = np.std(signal) + 1e-8
     return (signal - mean) / std
 
+#Converts signal sampling rate to 250hz
 def resample_to(signal, fs_in, fs_out=250):
     if fs_in == fs_out:
         return signal, fs_in
     n_out = int(len(signal) * fs_out / fs_in)
     return resample(signal, n_out), fs_out
 
+#Splits signals up into "Windows" that overlap by 1s to increase accuracy and in-case abnormality occurs on "border"
 def make_windows_fixed(signal, fs, win_sec=2.0, step_sec=1.0):
     win = int(win_sec * fs)     
     step = int(step_sec * fs)
