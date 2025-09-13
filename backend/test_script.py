2aad4ba4d426c9bd1330e1855e9ddeabdbb7c390
AMarques05 <05marquesanthony@gmail.com>
2025-09-12 21:26:59 -0400

    adjusted the model to be more strict it was too good what can i say hommies

--- backend/test_script.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-X_train = np.load("backend/data/processed/X_train.npy")
-X_val   = np.load("backend/data/processed/X_val.npy")
-
-print("Train shape:", X_train.shape)
-print("Val shape:", X_val.shape)
